436380530eacc0b41b2cec42b8f6632a57717001
Pepe Doval <pepellou@gmail.com>
2020-09-03 06:16:54 +0200

    Refactor

--- character_with_longest_consecutive_repetition/solution.js
+++ character_with_longest_consecutive_repetition/solution.js
@@ -1,24 +1,15 @@
 function longestRepetition(s) {
-    let min = [ "", 0 ],
-        count = 0,
-        current = "";
+    let min = [ "", 0 ], cnt = 0, cur = "";
 
-    for (let i = 0; i < s.length; i++) {
-        const c = s[i];
-        if (c == current) {
-            count++;
-        } else {
-            if (count > min[1]) {
-                min[0] = current;
-                min[1] = count;
+    for (let i = 0; i <= s.length; i++) {
+        if (s[i] != cur) {
+            if (cnt > min[1]) {
+                min = [ cur, cnt ];
             }
-            count = 1;
-            current = c;
+            cur = s[i];
+            cnt = 0;
         }
-    }
-    if (count > min[1]) {
-        min[0] = current;
-        min[1] = count;
+        cnt++;
     }
 
     return min;
